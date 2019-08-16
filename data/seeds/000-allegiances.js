ddd11d939607fde4d454d90f055a963cc689d21b
vanman9599 <vanjordan46@hotmail.com>
2019-08-16 14:56:20 -0700

    feat/seeding

--- data/seeds/000-allegiances.js
+++ data/seeds/000-allegiances.js
@@ -5,9 +5,33 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('allegiances').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { allegiance_name: 'Las Vegas Raiders',sport: "NFL"},
+        { allegiance_name: 'Miami Dolphins',sport: "NFL"},
+        { allegiance_name: 'Tennessee Titans Fans', sport: "NFL"},
+        { allegiance_name: 'Buffalo Bills', sport: "NFL"},
+        { allegiance_name: 'New England Patriots Fans', sport: "NFL"},
+        { allegiance_name: 'New York Jets Fans', sport: "NFL"},
+        { allegiance_name: 'Denver Broncos Fans', sport: "NFL"},
+        { allegiance_name: 'Kansas City Chiefs Fans', sport: "NFL"},
+        { allegiance_name: 'LA Chargers Fans', sport: "NFL"},
+        { allegiance_name: 'Las Vegas Raiders Fans', sport: "NFL"},
+        { allegiance_name: 'Baltimore Ravens Fans', sport: "NFL"},
+        { allegiance_name: 'Cincinatti Bengals Fans',  sport: "NFL"},
+        { allegiance_name: 'Cleveland Browns Fans',  sport: "NFL"},
+        { allegiance_name: 'Pittsburgh Steelers Fans',  sport: "NFL"},
+        { allegiance_name: 'Houston Texans Fans',  sport: "NFL"},
+        { allegiance_name: 'Indianapolis Colts Fans', sport: "NFL"},
+        { allegiance_name: 'Jacksonville Jaguars Fans', sport: "NFL"},
+        { allegiance_name: 'Dallas Cowboys Fans', sport: "NFL"},
+        { allegiance_name: 'Washington Redskins Fans', sport: "NFL"},
+        { allegiance_name: 'New York Giants Fans', sport: "NFL"},
+        { allegiance_name: 'Arizona Cardinals Fans', sport: "NFL"},
+        { allegiance_name: 'LA Rams Fans', sport: "NFL"},
+        { allegiance_name: 'San Francisco 49ers Fans', sport: "NFL"},
+        { allegiance_name: 'Seattle Seahawks Fans', sport: "NFL"},
+        { allegiance_name: 'Chicago Bears Fans', sport: "NFL"},
+        { allegiance_name: 'Detroit Lions Fans', sport: "NFL"},
+        { allegiance_name: 'Green Bay Packers Fans', sport: "NFL"},
       ]);
     });
 };
--- data/seeds/004-groups-users.js
+++ data/seeds/004-groups-users.js
@@ -1,13 +1,38 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('groups_users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('groups_users').insert([
+        {user_id: 1, user_type: 'admin', group_id: 1},
+        {user_id: 1, user_type: 'admin', group_id: 2},
+        {user_id: 1, user_type: 'admin', group_id: 3},
+        {user_id: 2, user_type: 'admin', group_id: 4},
+        {user_id: 2, user_type: 'admin', group_id: 5},
+        {user_id: 2, user_type: 'admin', group_id: 6},
+        {user_id: 3, user_type: 'admin', group_id: 7},
+        {user_id: 3, user_type: 'admin', group_id: 8},
+        {user_id: 3, user_type: 'admin', group_id: 9},
+        {user_id: 4, user_type: 'admin', group_id: 10},
+        {user_id: 4, user_type: 'admin', group_id: 11},
+        {user_id: 4, user_type: 'admin', group_id: 12},
+        {user_id: 5, user_type: 'admin', group_id: 13},
+        {user_id: 5, user_type: 'admin', group_id: 14},
+        {user_id: 5, user_type: 'admin', group_id: 15},
+        {user_id: 6, user_type: 'admin', group_id: 16},
+        {user_id: 6, user_type: 'admin', group_id: 17},
+        {user_id: 6, user_type: 'admin', group_id: 18},
+        {user_id: 7, user_type: 'admin', group_id: 19},
+        {user_id: 7, user_type: 'admin', group_id: 20},
+        {user_id: 7, user_type: 'admin', group_id: 21},
+        {user_id: 8, user_type: 'admin', group_id: 22},
+        {user_id: 8, user_type: 'admin', group_id: 23},
+        {user_id: 8, user_type: 'admin', group_id: 24},
+        {user_id: 9, user_type: 'admin', group_id: 25},
+
+
+
       ]);
     });
 };
