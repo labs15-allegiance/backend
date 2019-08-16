9a03515deaece5986ac93c87e7fa3c98c02c0566
Derek S <dschwan21@me.com>
2019-08-16 13:56:33 -0700

    built endpoints for get user and add user to pass to front end

---
+++ routers/users-model.js
@@ -0,0 +1,38 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function find() {
+  return db("users").select(
+    "id",
+    "username",
+    "location",
+    "email",
+    "image",
+    "banner_image"
+  );
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
