d4566e3608955e9b2878969d73b595c80e67ab57
vanman9599 <vanjordan46@hotmail.com>
2019-08-17 06:27:11 -0700

    added seeds for baseball

--- data/seeds/001-allegiances.js
+++ data/seeds/001-allegiances.js
@@ -4,6 +4,7 @@ exports.seed = function(knex) {
   
       // Inserts seed entries
       return knex('allegiances').insert([
+        // NFL TEAMS
         { allegiance_name: 'Las Vegas Raiders',sport: "NFL"},
         { allegiance_name: 'Miami Dolphins', sport: "NFL"},
         { allegiance_name: 'Tennessee Titans', sport: "NFL"},
@@ -35,6 +36,38 @@ exports.seed = function(knex) {
         { allegiance_name: 'Carolina Panthers', sport: "NFL"},
         { allegiance_name: 'New Orleans Saints', sport: "NFL"},
         { allegiance_name: 'Tampa Bay Buccaneers', sport: "NFL"},
+        //MLB Teams
+        { allegiance_name: 'Atlanta Braves', sport: "MLB"},
+        { allegiance_name: 'Miami Marlins', sport: "MLB"},
+        { allegiance_name: 'New York Mets', sport: "MLB"},
+        { allegiance_name: 'Philadelphia Phillies', sport: "MLB"},
+        { allegiance_name: 'Washington Nationals', sport: "MLB"},
+        { allegiance_name: 'Chicago Cubs', sport: "MLB"},
+        { allegiance_name: 'Cincinnati Reds', sport: "MLB"},
+        { allegiance_name: 'Milwaukee Brewers', sport: "MLB"},
+        { allegiance_name: 'Pittsburgh Pirates', sport: "MLB"},
+        { allegiance_name: 'St. Louis Cardinals', sport: "MLB"},
+        { allegiance_name: 'Arizona Diamondbacks', sport: "MLB"},
+        { allegiance_name: 'Colorado Rockies', sport: "MLB"},
+        { allegiance_name: 'Los Angeles Dodgers', sport: "MLB"},
+        { allegiance_name: 'San Diego Padres', sport: "MLB"},
+        { allegiance_name: 'San Francisco Giants', sport: "MLB"},
+        { allegiance_name: 'Baltimore Orioles', sport: "MLB"},
+        { allegiance_name: 'Boston Red Sox', sport: "MLB"},
+        { allegiance_name: 'New York Yankees', sport: "MLB"},
+        { allegiance_name: 'Tampa Bay Rays', sport: "MLB"},
+        { allegiance_name: 'Toronto Blue Jays', sport: "MLB"},
+        { allegiance_name: 'Chicago White Sox', sport: "MLB"},
+        { allegiance_name: 'Cleveland Indians', sport: "MLB"},
+        { allegiance_name: 'Detroit Tigers', sport: "MLB"},
+        { allegiance_name: 'Kansas City Royals', sport: "MLB"},
+        { allegiance_name: 'Minnesota Twins', sport: "MLB"},
+        { allegiance_name: 'Houston Astros', sport: "MLB"},
+        { allegiance_name: 'Los Angeles Angels', sport: "MLB"},
+        { allegiance_name: 'Oakland Athletics', sport: "MLB"},
+        { allegiance_name: 'Seattle Mariners', sport: "MLB"},
+        { allegiance_name: 'Texas Rangers', sport: "MLB"}
+
 
       ]);
     
