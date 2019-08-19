6df7481b5be6139627d00b000d50b95bede27134
Ang Xu <angxu1234@gmail.com>
2019-08-19 17:43:34 -0500

    merge resolution

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require("./api/server");
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+	console.log(`Listening on port ${PORT}...`);
 });
