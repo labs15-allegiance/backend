8b1122b711702da2e32816f64e0a0432ee3b0884
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-08-19 08:48:24 -0500

    :art: Controller follows router.route() pattern
    Each endpoint for same path such as _/_ attached to same router instance for readability

--- controllers/user.js
+++ controllers/user.js
@@ -4,24 +4,25 @@ const Users = require("../models/users");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const newUser = await req.body;
-    console.log("trying to add user:", newUser);
-    Users.add(newUser).then(user => {
-      res.status(201).json(user);
-    });
-  } catch (error) {
-    res.status(500).json({ error: "Error trying to add new user" });
-  }
-});
+router
+  .route("/")
+  .get((req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  })
+  .post(async (req, res) => {
+    try {
+      const newUser = await req.body;
+      console.log("trying to add user:", newUser);
+      Users.add(newUser).then(user => {
+        res.status(201).json(user);
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Error trying to add new user" });
+    }
+  });
 
 module.exports = router;
