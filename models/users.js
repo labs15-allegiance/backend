eb88a1492163c08e04619a1c9b67e2c36b314487
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-08-19 11:07:40 -0500

    :speech_balloon: Add comments

--- models/users.js
+++ models/users.js
@@ -14,6 +14,8 @@ function add(user) {
 }
 
 function find(filters) {
+  // if filters were passed in, search by filter. otherwise return all
+  // note that neither return use the .first() method -- it's on a use-by-use basis if that is required or not
   if (filters) {
     return db("users")
       .select("id", "username", "location", "email", "image", "banner_image")
@@ -30,6 +32,7 @@ function find(filters) {
 }
 
 function update(filter, changes) {
+  // only allow one update at a time, so uses .first()
   return db("users")
     .update(changes, ["*"])
     .where(filter)
@@ -38,6 +41,7 @@ function update(filter, changes) {
 }
 
 function remove(filter) {
+  // only returns the number of deleted entries
   return db("users")
     .where(filter)
     .del();
