c03d19722e63533594df07e89c150c72b543f843
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-08-19 09:44:35 -0500

    :bug: Passwords removed from seeds
    Password not included in updated version of data model for users

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -1,17 +1,15 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-        // Inserts seed entries
-      return knex('users').insert([
-        {username: 'micheal', email: 'bob@gmail.com', password: 'pass', location: 89103},
-        {username: 'jarred', email: 'jarred@gmail.com', password: 'pass', location: 23602},
-        {username: 'ang', email: 'ang@gmail.com', password: 'pass', location: 33014},
-        {username: 'angel', email: 'angel@gmail.com', password: 'pass', location: 92704},
-        {username: 'john', email: 'john@gmail.com', password: 'pass', location: 89104},
-        {username: 'dan', email: 'dan@gmail.com', password: 'pass', location: 89104},
-        {username: 'adam', email: 'adam@gmail.com', password: 'pass', location: 89104},
-        {username: 'van', email: 'larry@gmail.com', password: 'pass', location: 89104},
-        {username: 'cassandra', email: 'cassandra@gmail.com', password: 'pass', location: 89104},
-      ]);
-    
+  // Inserts seed entries
+  return knex("users").insert([
+    { username: "micheal", email: "bob@gmail.com", location: 89103 },
+    { username: "jarred", email: "jarred@gmail.com", location: 23602 },
+    { username: "ang", email: "ang@gmail.com", location: 33014 },
+    { username: "angel", email: "angel@gmail.com", location: 92704 },
+    { username: "john", email: "john@gmail.com", location: 89104 },
+    { username: "dan", email: "dan@gmail.com", location: 89104 },
+    { username: "adam", email: "adam@gmail.com", location: 89104 },
+    { username: "van", email: "larry@gmail.com", location: 89104 },
+    { username: "cassandra", email: "cassandra@gmail.com", location: 89104 }
+  ]);
 };
