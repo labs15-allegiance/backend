8e1d79c5df805cb61dc37a1fc4d956be1743432d
Lambda_School_Loaner_13 <lambda_school_loaner_13@Lambda-School-Loaner-13s-MacBook-Pro.local>
2019-08-20 18:47:54 -0400

    find and add group functions created

--- models/groups.js
+++ models/groups.js
@@ -9,3 +9,34 @@ module.exports = {
   addTask,
 };
 
+function find() {
+    return db('groups');
+  }
+
+// async function findById(id) {
+//     const event = await db('groups')
+//       .where({ id })
+//       .first();
+  
+//     if (group) {
+//       return db('tasks')
+//         .where({ group_id: id })
+//         .then(tasks => {
+//           group.tasks = tasks.map(t => ({ ...t, done: mapBoolean(t.done) }));
+//           return group;
+//         });
+//     } else {
+//       return null;
+//     }
+// }
+
+async function add(group) {
+    try {
+      const [id] = await db('groups').insert(group, 'id');
+  
+      return findById(id);
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+  
