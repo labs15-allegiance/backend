1cc052b08b3f3a9bb716103cb9437011d867ccf8
danpatrickoneill <dan47573@gmail.com>
2019-08-20 21:46:50 -0400

    Added validation middleware to user POST call

--- controllers/user.js
+++ controllers/user.js
@@ -4,13 +4,16 @@ const Users = require("../models/users");
 
 const router = express.Router();
 
+const validation = require("../middleware/dataValidation");
+const { userSchema } = require("../schemas");
+
 router
   .route("/")
   .get(async (req, res) => {
     const users = await Users.find();
     res.status(200).json({ users });
   })
-  .post(async (req, res) => {
+  .post(validation(userSchema), async (req, res) => {
     const newUser = await Users.add(req.body);
     console.log("trying to add user:", newUser);
     res.status(201).json({ newUser });
