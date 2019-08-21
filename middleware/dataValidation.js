cab60945b255abe68b8820c11c3145d6fec8d292
danpatrickoneill <dan47573@gmail.com>
2019-08-20 20:51:16 -0400

    Added data validation middleware using Joi schemas

---
+++ middleware/dataValidation.js
@@ -0,0 +1,17 @@
+const Joi = require("@hapi/joi");
+
+// Takes in a schema, checks req body against it, and returns middleware that throws a 400 or moves on to the next
+module.exports = schema => {
+  return (req, res, next) => {
+    const { error } = Joi.validate(req.body, schema);
+    if (error) {
+      console.log(error);
+      return res.status(400).json({
+        error: `Error during ${req.method} at ${req.originalUrl}: ${
+          error.details[0].message
+        }`
+      });
+    }
+    next();
+  };
+};
