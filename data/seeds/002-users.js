7070bb13f87e5eb95eb87bbd89e8424961f1962d
Derek S <dschwan21@me.com>
2019-08-21 13:45:40 -0700

    changed zipcode to be only 5 characters

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -3,22 +3,18 @@ const faker = require("faker");
 const createFakeUser = () => ({
   username: faker.internet.userName(),
   email: faker.internet.email(),
-  location: faker.address.zipCode(),
-  first_name: faker.name.firstName(), 
+  location: parseInt(faker.address.zipCode("#####")),
+  first_name: faker.name.firstName(),
   last_name: faker.name.lastName(),
   bio: faker.lorem.sentences()
-
-
-})
-exports.seed =  async function(knex, Promise) {
+});
+exports.seed = async function(knex, Promise) {
   // Deletes ALL existing entries
-        // Inserts seed entries
-      const fakeUsers = [];
-      const count = 500;
-      for(let i =0; i< count; i++){
-        fakeUsers.push(createFakeUser());
-      }
-      await knex('users')
-      .insert(fakeUsers);
-  
+  // Inserts seed entries
+  const fakeUsers = [];
+  const count = 500;
+  for (let i = 0; i < count; i++) {
+    fakeUsers.push(createFakeUser());
+  }
+  await knex("users").insert(fakeUsers);
 };
