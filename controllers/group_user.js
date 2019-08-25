05e84b78ac9ab61aa6bfe54043ca7b7fbf571b29
Ang Xu <angxu1234@gmail.com>
2019-08-25 07:37:25 -0500

    :sparkles: Adding group_user model and controller
    Added model for group_user table and began controller code for the model - will use to allow users to join groups

---
+++ controllers/group_user.js
@@ -0,0 +1,66 @@
+const express = require("express");
+
+const GroupUsers = require("../models/groups_users");
+
+const router = express.Router();
+
+const validation = require("../middleware/dataValidation");
+const { groupUsersSchema } = require("../schemas");
+
+router
+	.route("/")
+	.get(async (req, res) => {
+		const groupUsers = await GroupUsers.find();
+		res.status(200).json({
+			groupUsers
+		});
+	})
+	.post(validation(groupUsersSchema), async (req, res) => {
+		const newGroupUsers = await GroupUsers.add(req.body);
+		res.status(201).json({
+			newGroupUsers
+		});
+	});
+
+router
+	.route("/:id")
+	.put(validation(groupUsersSchema), async (req, res) => {
+		const { id } = req.params;
+		const changes = req.body;
+		const relationExists = await GroupUsers.find({ id }).first();
+		if (!relationExists) {
+			res
+				.status(404)
+				.json({ message: "That user to group pairing does not exist." });
+		} else {
+			const updated = await GroupUsers.update({ id }, changes);
+			res.status(200).json({ updated });
+		}
+	})
+	.delete(async (req, res) => {
+		const { id } = req.params;
+
+		const deleted = await GroupUsers.remove({ id });
+		if (deleted) {
+			res
+				.status(200)
+				.json({ message: "The user to group pairing has been deleted." });
+		} else {
+			res
+				.status(404)
+				.json({ message: "That user to group pairing does not exist." });
+		}
+	})
+	.get(async (req, res) => {
+		const { id } = req.params;
+		const groupUsers = await GroupUsers.find({ id }).first();
+		if (groupUsers && groupUsers.id) {
+			res.status(200).json({ groupUsers });
+		} else {
+			res
+				.status(404)
+				.json({ message: "That user to group pairing does not exist." });
+		}
+	});
+
+module.exports = router;
---
+++ models/groups_users.js
@@ -0,0 +1,83 @@
+const db = require("../data/db-config");
+
+module.exports = {
+	add,
+	find,
+	update,
+	remove
+};
+
+function add(group_user) {
+	return (
+		db("groups_users")
+			// user_id, user_type, group_id required
+			.insert(group_user, ["*"])
+			.then(g_u => find({ id: g_u[0].id }).first())
+	);
+}
+
+function find(filters) {
+	if (filters) {
+		return db("groups_users as g_u")
+			.leftJoin("users as u", "u.id", "g_u.user_id")
+			.leftJoin("groups as g", "g.id", "g_u.group_id")
+			.where(filters)
+			.select(
+				"g_u.id",
+				"u.id",
+				"u.first_name",
+				"u.last_name",
+				"u.username",
+				"u.email",
+				"u.location as user_location",
+				"u.image as user_image",
+				"g.id",
+				"g.group_name",
+				"g.creator_id",
+				"g.privacy_setting",
+				"g.location as group_location",
+				"g.image as group_image",
+				"g_u.timestamps"
+			);
+	} else {
+		return db("groups_users as g_u")
+			.leftJoin("users as u", "u.id", "g_u.user_id")
+			.leftJoin("groups as g", "g.id", "g_u.group_id")
+			.select(
+				"g_u.id",
+				"u.id",
+				"u.username",
+				"u.email",
+				"u.location as user_location",
+				"u.first_name",
+				"u.last_name",
+				"u.image as user_image",
+				"g.id",
+				"g.group_name",
+				"g.privacy_setting",
+				"g.location as group_location",
+				"g.creator_id",
+				"g.image as group_image",
+				"g_u.timestamps"
+			);
+	}
+}
+
+function update(filter, changes) {
+	// only allow one update at a time, so uses .first()
+	return db("groups_users")
+		.update(changes, ["*"])
+		.where(filter)
+		.then(g_u =>
+			find({
+				id: g_u[0].id
+			}).first()
+		);
+}
+
+function remove(filter) {
+	// only returns the number of deleted entries
+	return db("groups_users")
+		.where(filter)
+		.del();
+}
