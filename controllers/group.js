2e98e41aac3cb5b4bc65247c701ae45e9dca02bb
Derek S <dschwan21@me.com>
2019-08-26 16:59:07 -0700

    fixed search function

--- controllers/group.js
+++ controllers/group.js
@@ -12,109 +12,109 @@ const validation = require("../middleware/dataValidation");
 const { groupSchema } = require("../schemas");
 
 router
-	.route("/")
-	.get(async (req, res) => {
-		const groups = await Groups.find();
-		res.status(200).json({ groups });
-	})
-	.post(validation(groupSchema), async (req, res) => {
-		const { creator_id } = req.body;
-		const user = await Users.find({
-			id: creator_id
-		}).first();
-		if (user) {
-			const newGroup = await Groups.add(req.body);
+  .route("/")
+  .get(async (req, res) => {
+    const groups = await Groups.find();
+    res.status(200).json({ groups });
+  })
+  .post(validation(groupSchema), async (req, res) => {
+    const { creator_id } = req.body;
+    const user = await Users.find({
+      id: creator_id
+    }).first();
+    if (user) {
+      const newGroup = await Groups.add(req.body);
 
-			res.status(201).json({
-				newGroup
-			});
-		} else {
-			res.status(404).json({
-				message: "the creator of this group does not exist"
-			});
-		}
-	});
+      res.status(201).json({
+        newGroup
+      });
+    } else {
+      res.status(404).json({
+        message: "the creator of this group does not exist"
+      });
+    }
+  });
 
 // endpoint to retrieve groups for fuzzy search
 router.route("/search").post(async (req, res) => {
-	if (req.body.column !== undefined && req.body.row !== undefined) {
-		if (req.body.column === "location") {
-			// Takes zip code and optional radius from request body
-			const zip = req.body.row;
-			const rad = 10 || req.body.radius;
+  if (req.body.column !== undefined && req.body.row !== undefined) {
+    if (req.body.column === "location") {
+      // Takes zip code and optional radius from request body
+      const zip = req.body.row;
+      const rad = 10 || req.body.radius;
 
-			// Returns an array of zipcodes within mile radius of the zip
-			req.body.row = zipcodes.radius(zip, rad);
-		}
+      // Returns an array of zipcodes within mile radius of the zip
+      req.body.row = zipcodes.radius(zip, rad);
+    }
 
-		const groupByFilter = await Groups.find(req.body);
-		console.log("getting groups");
-		res.status(200).json({
-			groupByFilter
-		});
-	} else {
-		res.status(400).json({ message: "Column and Row must be provided." });
-	}
+    const groupByFilter = await Groups.search(req.body);
+    console.log("getting groups");
+    res.status(200).json({
+      groupByFilter
+    });
+  } else {
+    res.status(400).json({ message: "Column and Row must be provided." });
+  }
 });
 
 router
-	.route("/:id")
-	.put(validation(groupSchema), async (req, res) => {
-		const { id } = req.params;
-		const changes = req.body;
-		const userExists = await Users.find({
-			id: req.body.creator_id
-		}).first();
-		if (!userExists) {
-			res.status(404).json({ message: "User cannot be found" });
-		}
-		const groupExists = await Groups.find({ id }).first();
-		if (!groupExists) {
-			res.status(404).json({ message: "That group does not exist." });
-		}
+  .route("/:id")
+  .put(validation(groupSchema), async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    const userExists = await Users.find({
+      id: req.body.creator_id
+    }).first();
+    if (!userExists) {
+      res.status(404).json({ message: "User cannot be found" });
+    }
+    const groupExists = await Groups.find({ id }).first();
+    if (!groupExists) {
+      res.status(404).json({ message: "That group does not exist." });
+    }
 
-		const updated = await Groups.update({ id }, changes);
-		res.status(200).json({
-			updated
-		});
-	})
-	.delete(async (req, res) => {
-		const { id } = req.params;
-		const deleted = await Groups.remove({ id });
-		if (deleted) {
-			res.status(200).json({
-				message: "Group successfully deleted."
-			});
-		} else {
-			res.status(404).json({ message: "That group does not exist." });
-		}
-	})
-	.get(async (req, res) => {
-		const { id } = req.params;
-		const group = await Groups.find({ id }).first();
-		const allegianceCall = await GroupsAllegiances.find({ group_id: id });
-		const allegiances = allegianceCall.map(allegiance => {
-			const {
-				allegiance_id,
-				allegiance_name,
-				allegiance_image,
-				sport
-			} = allegiance;
-			return {
-				id: allegiance_id,
-				name: allegiance_name,
-				image: allegiance_image,
-				sport
-			};
-		});
-		if (group && group.id) {
-			res.status(200).json({
-				group,
-				allegiances
-			});
-		} else {
-			res.status(404).json({ message: "That group does not exist." });
-		}
-	});
+    const updated = await Groups.update({ id }, changes);
+    res.status(200).json({
+      updated
+    });
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    const deleted = await Groups.remove({ id });
+    if (deleted) {
+      res.status(200).json({
+        message: "Group successfully deleted."
+      });
+    } else {
+      res.status(404).json({ message: "That group does not exist." });
+    }
+  })
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const group = await Groups.find({ id }).first();
+    const allegianceCall = await GroupsAllegiances.find({ group_id: id });
+    const allegiances = allegianceCall.map(allegiance => {
+      const {
+        allegiance_id,
+        allegiance_name,
+        allegiance_image,
+        sport
+      } = allegiance;
+      return {
+        id: allegiance_id,
+        name: allegiance_name,
+        image: allegiance_image,
+        sport
+      };
+    });
+    if (group && group.id) {
+      res.status(200).json({
+        group,
+        allegiances
+      });
+    } else {
+      res.status(404).json({ message: "That group does not exist." });
+    }
+  });
 
 module.exports = router;
