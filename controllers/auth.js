ca7f02f20b31c188277313b6323a50e1d64c831c
Derek S <dschwan21@me.com>
2019-08-26 14:32:06 -0700

    added testing for auth, changed auth router to handle 400 error in testing environment

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,15 +5,19 @@ const Users = require("../models/users");
 const router = express.Router();
 
 router.route("/").post(async (req, res) => {
-  const currentUser = await Users.find({ email: req.body.email }).first();
-  if (currentUser) {
-    console.log("loggin in");
-    res.status(200).json({ currentUser });
+  if (req.body.email === undefined) {
+    res.status(400).json({ message: "please include email in the request" });
   } else {
-    const newUser = await Users.add(req.body);
-    console.log(req.body);
-    console.log("trying to add user:", newUser);
-    res.status(201).json({ newUser });
+    const currentUser = await Users.find({ email: req.body.email }).first();
+    if (currentUser) {
+      console.log("loggin in");
+      res.status(200).json({ currentUser });
+    } else {
+      const newUser = await Users.add(req.body);
+      console.log(req.body);
+      console.log("trying to add user:", newUser);
+      res.status(201).json({ newUser });
+    }
   }
 });
 
