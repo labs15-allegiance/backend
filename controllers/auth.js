fb21d8fc21eb8ea420a0007908f305c6278a152d
JarredStanford <stanj89@gmail.com>
2019-08-28 13:04:59 -0400

    login now sends the user's groups as well.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,32 @@
 const express = require("express");
 
 const Users = require("../models/users");
+const GroupsUsers = require("../models/groups_users");
 
 const router = express.Router();
 
 router.route("/").post(async (req, res) => {
-  if (req.body.email === undefined) {
-    res.status(400).json({ message: "please include email in the request" });
+  const currentUser = await Users.find({ email: req.body.email }).first();
+  if (currentUser) {
+    const userGroups = await GroupsUsers.find({ user_id: currentUser.id });
+    if (userGroups) {
+      const basicGroupInfo = userGroups.map(group => {
+        return (
+          {
+            "name": group.group_name,
+            "image": group.group_image,
+            "id": group.group_id
+          }
+        )
+      })
+      return res.status(200).json({ currentUser, basicGroupInfo });
+    } else res.status(200).json({ currentUser });
+    console.log("loggin in");
   } else {
-    const currentUser = await Users.find({ email: req.body.email }).first();
-    if (currentUser) {
-      console.log("loggin in");
-      res.status(200).json({ currentUser });
-    } else {
-      const newUser = await Users.add(req.body);
-      console.log(req.body);
-      console.log("trying to add user:", newUser);
-      res.status(201).json({ newUser });
-    }
+    const newUser = await Users.add(req.body);
+    console.log(req.body);
+    console.log("trying to add user:", newUser);
+    res.status(201).json({ newUser });
   }
 });
 
