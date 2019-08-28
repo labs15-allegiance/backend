08ac60ec2db7511d1f7742b6bc54ad304da40c38
danpatrickoneill <dan47573@gmail.com>
2019-08-28 19:06:29 -0400

    Removed check for city/state input; zip validation

--- controllers/group.js
+++ controllers/group.js
@@ -42,29 +42,34 @@ const findColumns = ["id", "creator_id", "privacy_setting"];
 router.route("/search").post(async (req, res) => {
   if (req.body.column === "location") {
     // Ternary check for zip code or text; zip gets passed along as is, city + state gets converted. city and state should be provided as object with those keys
-    const zip = isNaN(req.body.row)
-      ? zipcodes.lookupByName(req.body.row.city, req.body.row.state)[0].zip
-      : req.body.row;
+    if (zipcodes.lookup(req.body.row)) {
+      const zip = req.body.row;
 
-    console.log(zip);
-    // Takes optional radius from request or sets default
-    const rad = 10 || req.body.radius;
+      console.log(zip);
+      // Takes optional radius from request or sets default
+      const rad = 10 || req.body.radius;
 
-    // Returns an array of zipcodes within mile radius of the zip
-    req.body.row = zipcodes.radius(zip, rad);
+      // Returns an array of zipcodes within mile radius of the zip
+      req.body.row = zipcodes.radius(zip, rad);
 
-    const groupByFilter = await Groups.find(req.body);
-    console.log("getting groups");
+      const groupByFilter = await Groups.find(req.body);
+      console.log("getting groups");
 
-    // Sort results by smallest to largest distance as the crow flies
-    groupByFilter.sort(
-      (a, b) =>
-        zipcodes.distance(a.location, zip) - zipcodes.distance(b.location, zip)
-    );
+      // Sort results by smallest to largest distance as the crow flies
+      groupByFilter.sort(
+        (a, b) =>
+          zipcodes.distance(a.location, zip) -
+          zipcodes.distance(b.location, zip)
+      );
 
-    res.status(200).json({
-      groupByFilter
-    });
+      res.status(200).json({
+        groupByFilter
+      });
+    } else {
+      res.status(400).json({
+        error: `Error during ${req.method} at ${req.originalUrl}: Please provide valid zip code`
+      });
+    }
   }
   // Branch for columns needing strict find
   else if (findColumns.includes(req.body.column)) {
