bb92f49b87bfe160cd7d637618a37c8af94229e7
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-09-03 17:18:44 -0500

    :bug: Adds 404 cases to all /:id endpoints, tested

--- controllers/allegiance.js
+++ controllers/allegiance.js
@@ -19,18 +19,27 @@ router
   .route("/:id")
   .put(async (req, res) => {
     const { id } = req.params;
+    const allegianceExists = await Allegiances.find({ id }).first();
+    if (!allegianceExists) {
+      res.status(404).json({ message: "That resource does not exist." });
+    }
     const updated = await Allegiances.update({ id }, req.body);
     res.status(200).json({ updated });
   })
   .delete(async (req, res) => {
     const { id } = req.params;
+
     const deleted = await Allegiances.remove({ id });
-    res.status(200).json({ deleted });
+    deleted
+      ? res.status(200).json({ deleted })
+      : res.status(404).json({ message: "That resource does not exist." });
   })
   .get(async (req, res) => {
     const { id } = req.params;
     const allegiance = await Allegiances.find({ id }).first();
-    res.status(200).json({ allegiance });
+    allegiance
+      ? res.status(200).json({ allegiance })
+      : res.status(404).json({ message: "That resource does not exist." });
   });
 
 module.exports = router;
