1f77b87a7edb65465d3c74696638e4f71adeac21
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-09-03 16:28:49 -0500

    :art: Adds .first() onto get/:id call, cleans endpoint
    Previously returned one resource in an array without .first()

--- controllers/allegiance.js
+++ controllers/allegiance.js
@@ -5,37 +5,35 @@ const Allegiances = require("../models/allegiances");
 const router = express.Router();
 
 router
-	.route("/")
-	.get(async (req, res) => {
-		const allegiances = await Allegiances.find();
-		res.status(200).json({ allegiances });
-	})
-	.post(async (req, res) => {
-		const newAllegiance = await Allegiances.add(req.body);
-		console.log("trying to add allegiance:", newAllegiance);
-		res.status(201).json({ newAllegiance });
-	});
+  .route("/")
+  .get(async (req, res) => {
+    const allegiances = await Allegiances.find();
+    res.status(200).json({ allegiances });
+  })
+  .post(async (req, res) => {
+    const newAllegiance = await Allegiances.add(req.body);
+    res.status(201).json({ newAllegiance });
+  });
 
 router
-	.route("/:id")
-	.put(async (req, res) => {
-		const { id } = req.params;
-		const changes = req.body;
-		const filter = { id: id };
-		const updated = await Allegiances.update(filter, changes);
-		res.status(200).json({ updated });
-	})
-	.delete(async (req, res) => {
-		const { id } = req.params;
-		const filter = { id: id };
-		const deleted = await Allegiances.remove(filter);
-		res.status(200).json({ deleted });
-	})
-	.get(async (req, res) => {
-		const { id } = req.params;
-		const filter = { id: id };
-		const allegiance = await Allegiances.find(filter);
-		res.status(200).json({ allegiance });
-	});
+  .route("/:id")
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    const filter = { id: id };
+    const updated = await Allegiances.update(filter, changes);
+    res.status(200).json({ updated });
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    const filter = { id: id };
+    const deleted = await Allegiances.remove(filter);
+    res.status(200).json({ deleted });
+  })
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const allegiance = await Allegiances.find({ id }).first();
+    res.status(200).json({ allegiance });
+  });
 
 module.exports = router;
