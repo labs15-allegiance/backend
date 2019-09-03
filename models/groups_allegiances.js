74bc04f1ae7bc6c114aee0939141370cf652e99d
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-09-03 17:04:38 -0500

    :bug: Fixed issue: "id" was too ambiguous in db.add
    filter function for groups_allegiances.add() was too ambigous in its chained .find function, needed "id" to be "g_a.id"

--- models/groups_allegiances.js
+++ models/groups_allegiances.js
@@ -1,67 +1,67 @@
 const db = require("../data/db-config");
 
 module.exports = {
-	add,
-	find,
-	update,
-	remove
+  add,
+  find,
+  update,
+  remove
 };
 
 function add(group_allegiance) {
-	return (
-		db("groups_allegiances")
-			// allegiance_id, group_id required
-			.insert(group_allegiance, ["*"])
-			.then(g_a => find({ id: g_a[0].id }).first())
-	);
+  return (
+    db("groups_allegiances")
+      // allegiance_id, group_id required
+      .insert(group_allegiance, ["*"])
+      .then(g_a => find({ "g_a.id": g_a[0].id }).first())
+  );
 }
 
 function find(filters) {
-	if (filters) {
-		return db("groups_allegiances as g_a")
-			.leftJoin("allegiances as a", "a.id", "g_a.allegiance_id")
-			.leftJoin("groups as g", "g.id", "g_a.group_id")
-			.where(filters)
-			.select(
-				"g_a.id as id",
-				"g.id as group_id",
-				"group_name",
-				"a.id as allegiance_id",
-				"a.allegiance_name as allegiance_name",
-				"a.image as allegiance_image",
-				"a.sport as sport"
-			);
-	} else {
-		return db("groups_allegiances as g_a")
-			.leftJoin("allegiances as a", "a.id", "g_a.allegiance_id")
-			.leftJoin("groups as g", "g.id", "g_a.group_id")
-			.select(
-				"g_a.id as id",
-				"g.id as group_id",
-				"group_name",
-				"a.id as allegiance_id",
-				"a.allegiance_name as allegiance_name",
-				"a.image as allegiance_image",
-				"a.sport as sport"
-			);
-	}
+  if (filters) {
+    return db("groups_allegiances as g_a")
+      .leftJoin("allegiances as a", { "a.id": "g_a.allegiance_id" })
+      .leftJoin("groups as g", { "g.id": "g_a.group_id" })
+      .where(filters)
+      .select(
+        "g_a.id as id",
+        "g.id as group_id",
+        "group_name",
+        "a.id as allegiance_id",
+        "a.allegiance_name as allegiance_name",
+        "a.image as allegiance_image",
+        "a.sport as sport"
+      );
+  } else {
+    return db("groups_allegiances as g_a")
+      .leftJoin("allegiances as a", { "a.id": "g_a.allegiance_id" })
+      .leftJoin("groups as g", { "g.id": "g_a.group_id" })
+      .select(
+        "g_a.id as id",
+        "g.id as group_id",
+        "group_name",
+        "a.id as allegiance_id",
+        "a.allegiance_name as allegiance_name",
+        "a.image as allegiance_image",
+        "a.sport as sport"
+      );
+  }
 }
 
 function update(filter, changes) {
-	// only allow one update at a time, so uses .first()
-	return db("groups_allegiances")
-		.update(changes, ["*"])
-		.where(filter)
-		.then(g_a =>
-			find({
-				id: g_a[0].id
-			}).first()
-		);
+  // only allow one update at a time, so uses .first()
+  return db("groups_allegiances")
+    .update(changes, ["*"])
+    .where(filter)
+    .then(g_a =>
+      find({
+        id: g_a[0].id
+      }).first()
+    );
 }
 
 function remove(filter) {
-	// only returns the number of deleted entries
-	return db("groups_allegiances")
-		.where(filter)
-		.del();
+  // only returns the number of deleted entries
+  return db("groups_allegiances")
+    .where(filter)
+    .del();
 }
