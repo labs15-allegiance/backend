2df0cff6742f1aa6ea3f1956827b8e2f443180d1
Ang Xu <angxu1234@gmail.com>
2019-09-03 18:07:10 -0500

    :bug: Fix groupsusers ambiguous id
    Find and update ambiguous ids issues have been fixed, schema name updated for groupsusers

--- models/groups_users.js
+++ models/groups_users.js
@@ -12,41 +12,67 @@ function add(group_user) {
 		db("groups_users")
 			// user_id, user_type, group_id required
 			.insert(group_user, ["*"])
-			.then(g_u => find({ id: g_u[0].id }).first())
+			.then(g_u => find({ "g_u.id": g_u[0].id }).first())
 	);
 }
 
 function find(filters) {
 	if (filters) {
-		return db("groups_users as g_u")
-			.leftJoin("users as u", "u.id", "g_u.user_id")
-			.leftJoin("groups as g", "g.id", "g_u.group_id")
-			.where(filters)
-			.select(
-				"g_u.id",
-				"u.id as user_id",
-				"u.first_name",
-				"u.last_name",
-				"u.username",
-				"u.email",
-				"u.location as user_location",
-				"u.image as user_image",
-				"g_u.user_type",
-				"g.id as group_id",
-				"g.group_name",
-				"g.creator_id",
-				"g.privacy_setting",
-				"g.location as group_location",
-				"g.image as group_image",
-				"g_u.created_at",
-				"g_u.updated_at"
-			);
+		if (Array.isArray(filters.group_id)) {
+			return db("groups_users as g_u")
+				.leftJoin("users as u", "u.id", "g_u.user_id")
+				.leftJoin("groups as g", "g.id", "g_u.group_id")
+				.select(
+					"g_u.id as id",
+					"u.id as user_id",
+					"u.first_name",
+					"u.last_name",
+					"u.username",
+					"u.email",
+					"u.location as user_location",
+					"u.image as user_image",
+					"g_u.user_type",
+					"g.id as group_id",
+					"g.group_name",
+					"g.creator_id",
+					"g.privacy_setting",
+					"g.location as group_location",
+					"g.image as group_image",
+					"g_u.created_at",
+					"g_u.updated_at"
+				)
+				.whereIn("group_id", filters.group_id);
+		} else {
+			return db("groups_users as g_u")
+				.leftJoin("users as u", "u.id", "g_u.user_id")
+				.leftJoin("groups as g", "g.id", "g_u.group_id")
+				.where(filters)
+				.select(
+					"g_u.id as id",
+					"u.id as user_id",
+					"u.first_name",
+					"u.last_name",
+					"u.username",
+					"u.email",
+					"u.location as user_location",
+					"u.image as user_image",
+					"g_u.user_type",
+					"g.id as group_id",
+					"g.group_name",
+					"g.creator_id",
+					"g.privacy_setting",
+					"g.location as group_location",
+					"g.image as group_image",
+					"g_u.created_at",
+					"g_u.updated_at"
+				);
+		}
 	} else {
 		return db("groups_users as g_u")
 			.leftJoin("users as u", "u.id", "g_u.user_id")
 			.leftJoin("groups as g", "g.id", "g_u.group_id")
 			.select(
-				"g_u.id",
+				"g_u.id as id",
 				"u.id as user_id",
 				"u.username",
 				"u.email",
@@ -74,7 +100,7 @@ function update(filter, changes) {
 		.where(filter)
 		.then(g_u =>
 			find({
-				id: g_u[0].id
+				"g_u.id": g_u[0].id
 			}).first()
 		);
 }
