b61d4bd8aa6dc80b94ee9c7970b994f5e2d4a867
Ang Xu <angxu1234@gmail.com>
2019-09-18 18:47:28 -0500

    :sparkles: Adding better information for feed object
    Adding better information for feed object

--- controllers/feed.js
+++ controllers/feed.js
@@ -8,10 +8,48 @@ router.route("/").post(async (req, res) => {
 	const { group_id, interval } = req.body;
 	if (group_id && interval) {
 		const filters = { group_id, interval };
+		// Obtain posts from stated interval
 		const posts = await Feeds.findPosts(filters);
+		// Obtain replies from stated interval
 		const replies = await Feeds.findReplies(filters);
-		const postLikes = await Feeds.findPostLikes(filters);
-		const replyLikes = await Feeds.findReplyLikes(filters);
+		// Obtain post likes from stated interval
+		const postLikesSeparateNames = await Feeds.findPostLikes(filters);
+		// Map post likes to combine first and last name for posters and likers
+		const postLikes = postLikesSeparateNames.map(p_l => {
+			// Destructure to remove first/last names from callback
+			const {
+				liker_first_name,
+				liker_last_name,
+				poster_first_name,
+				poster_last_name,
+				...rest
+			} = p_l;
+			return {
+				// Return remainder of callback object and the combined names for posters and likers
+				...rest,
+				liker_name: `${p_l.liker_first_name} ${p_l.liker_last_name}`,
+				poster_name: `${p_l.poster_first_name} ${p_l.poster_last_name}`
+			};
+		});
+		// Obtain reply likes from stated interval
+		const replyLikesSeparateNames = await Feeds.findReplyLikes(filters);
+		// Map reply likes to combine first and last name for repliers and likers
+		const replyLikes = replyLikesSeparateNames.map(r_l => {
+			// Destructure to remove first/last names from callback
+			const {
+				liker_first_name,
+				liker_last_name,
+				replier_first_name,
+				replier_last_name,
+				...rest
+			} = r_l;
+			return {
+				// Return remainder of callback object and the combined names for repliers and likers
+				...rest,
+				liker_name: `${r_l.liker_first_name} ${r_l.liker_last_name}`,
+				replier_name: `${r_l.replier_first_name} ${r_l.replier_last_name}`
+			};
+		});
 		res.status(200).json({
 			posts,
 			replies,
--- models/feeds.js
+++ models/feeds.js
@@ -18,9 +18,11 @@ function findPosts(filters) {
 	if (Array.isArray(filters.group_id)) {
 		return db("posts as p")
 			.leftJoin("users as u", "u.id", "p.user_id")
+			.leftJoin("groups as g", "g.id", "p.group_id")
 			.select(
 				"p.id",
 				"u.id as user_id",
+				"g.group_name",
 				"p.group_id",
 				"p.post_content",
 				"p.created_at",
@@ -35,9 +37,11 @@ function findPosts(filters) {
 		// if single group is received, use where
 		return db("posts as p")
 			.leftJoin("users as u", "u.id", "p.user_id")
+			.leftJoin("groups as g", "g.id", "p.group_id")
 			.select(
 				"p.id",
 				"u.id as user_id",
+				"g.group_name",
 				"p.group_id",
 				"p.post_content",
 				"p.created_at",
@@ -66,6 +70,7 @@ function findReplies(filters) {
 				"p.group_id",
 				"u.id as user_id",
 				"r.post_id",
+				"p.post_content",
 				"r.reply_content",
 				"r.created_at",
 				"r.updated_at",
@@ -85,6 +90,7 @@ function findReplies(filters) {
 				"p.group_id",
 				"u.id as user_id",
 				"r.post_id",
+				"p.post_content",
 				"r.reply_content",
 				"r.created_at",
 				"r.updated_at",
@@ -104,42 +110,64 @@ function findPostLikes(filters) {
 	const ts = moment(Date.now()).subtract(n, "hours");
 	// If multiple groups are received, use whereIn
 	if (Array.isArray(filters.group_id)) {
-		return db("posts_likes as p_l")
-			.leftJoin("users as u", "u.id", "p_l.user_id")
-			.leftJoin("posts as p", "p.id", "p_l.post_id")
-			.select(
-				"p_l.id",
-				"p.group_id",
-				"p.post_content",
-				"u.id as user_id",
-				"p_l.post_id",
-				"p_l.created_at",
-				"p_l.updated_at",
-				"u.first_name",
-				"u.last_name",
-				"u.image"
-			)
-			.where("p_l.created_at", ">=", ts)
-			.whereIn("p.group_id", filters.group_id);
+		return (
+			db("posts_likes as p_l")
+				// liker shares user_id with postLikes table
+				.leftJoin("users as liker", "liker.id", "p_l.user_id")
+				.leftJoin("posts as p", "p.id", "p_l.post_id")
+				// poster shares user_id with posts table
+				.leftJoin("users as poster", "poster.id", "p.user_id")
+				.leftJoin("groups as g", "g.id", "p.group_id")
+				.select(
+					"p_l.id",
+					"p.group_id",
+					"g.group_name",
+					"p.post_content",
+					"liker.id as liker_id",
+					"poster.id as poster_id",
+					"p_l.post_id",
+					"p_l.created_at",
+					"p_l.updated_at",
+					"liker.first_name as liker_first_name",
+					"liker.last_name as liker_last_name",
+					"liker.image as liker_image",
+					"poster.first_name as poster_first_name",
+					"poster.last_name as poster_last_name",
+					"poster.image as poster_image"
+				)
+				.where("p_l.created_at", ">=", ts)
+				.whereIn("p.group_id", filters.group_id)
+		);
 	} else {
 		// if single group is received, use where
-		return db("posts_likes as p_l")
-			.leftJoin("users as u", "u.id", "p_l.user_id")
-			.leftJoin("posts as p", "p.id", "p_l.post_id")
-			.select(
-				"p_l.id",
-				"p.group_id",
-				"p.post_content",
-				"u.id as user_id",
-				"p_l.post_id",
-				"p_l.created_at",
-				"p_l.updated_at",
-				"u.first_name",
-				"u.last_name",
-				"u.image"
-			)
-			.where("p_l.created_at", ">=", ts)
-			.where("p.group_id", filters.group_id);
+		return (
+			db("posts_likes as p_l")
+				// liker shares user_id with postLikes table
+				.leftJoin("users as liker", "liker.id", "p_l.user_id")
+				.leftJoin("posts as p", "p.id", "p_l.post_id")
+				// poster shares user_id with posts table
+				.leftJoin("users as poster", "poster.id", "p.user_id")
+				.leftJoin("groups as g", "g.id", "p.group_id")
+				.select(
+					"p_l.id",
+					"p.group_id",
+					"g.group_name",
+					"p.post_content",
+					"liker.id as liker_id",
+					"poster.id as poster_id",
+					"p_l.post_id",
+					"p_l.created_at",
+					"p_l.updated_at",
+					"liker.first_name as liker_first_name",
+					"liker.last_name as liker_last_name",
+					"liker.image as liker_image",
+					"poster.first_name as poster_first_name",
+					"poster.last_name as poster_last_name",
+					"poster.image as poster_image"
+				)
+				.where("p_l.created_at", ">=", ts)
+				.where("p.group_id", filters.group_id)
+		);
 	}
 }
 
@@ -150,45 +178,63 @@ function findReplyLikes(filters) {
 	const ts = moment(Date.now()).subtract(n, "hours");
 	// If multiple groups are received, use whereIn
 	if (Array.isArray(filters.group_id)) {
-		return db("replies_likes as r_l")
-			.leftJoin("users as u", "u.id", "r_l.user_id")
-			.leftJoin("replies as r", "r.id", "r_l.reply_id")
-			.leftJoin("posts as p", "p.id", "r.post_id")
-			.select(
-				"r_l.id",
-				"p.group_id",
-				"p.post_content",
-				"r.reply_content",
-				"u.id as user_id",
-				"r_l.reply_id",
-				"r_l.created_at",
-				"r_l.updated_at",
-				"u.first_name",
-				"u.last_name",
-				"u.image"
-			)
-			.where("r_l.created_at", ">=", ts)
-			.whereIn("p.group_id", filters.group_id);
+		return (
+			db("replies_likes as r_l")
+				// liker shares user_id with replyLikes table
+				.leftJoin("users as liker", "liker.id", "r_l.user_id")
+				.leftJoin("replies as r", "r.id", "r_l.reply_id")
+				// replier shares user_id with replies table
+				.leftJoin("users as replier", "replier.id", "r.user_id")
+				.leftJoin("posts as p", "p.id", "r.post_id")
+				.select(
+					"r_l.id",
+					"p.group_id",
+					"p.post_content",
+					"r.reply_content",
+					"liker.id as liker_id",
+					"replier.id as replier_id",
+					"r_l.reply_id",
+					"r_l.created_at",
+					"r_l.updated_at",
+					"liker.first_name as liker_first_name",
+					"liker.last_name as liker_last_name",
+					"liker.image as liker_image",
+					"replier.first_name as replier_first_name",
+					"replier.last_name as replier_last_name",
+					"replier.image as replier_image"
+				)
+				.where("r_l.created_at", ">=", ts)
+				.whereIn("p.group_id", filters.group_id)
+		);
 	} else {
 		// if single group is received, use where
-		return db("replies_likes as r_l")
-			.leftJoin("users as u", "u.id", "r_l.user_id")
-			.leftJoin("replies as r", "r.id", "r_l.reply_id")
-			.leftJoin("posts as p", "p.id", "r.post_id")
-			.select(
-				"r_l.id",
-				"p.group_id",
-				"p.post_content",
-				"r.reply_content",
-				"u.id as user_id",
-				"r_l.reply_id",
-				"r_l.created_at",
-				"r_l.updated_at",
-				"u.first_name",
-				"u.last_name",
-				"u.image"
-			)
-			.where("r_l.created_at", ">=", ts)
-			.where("p.group_id", filters.group_id);
+		return (
+			db("replies_likes as r_l")
+				// liker shares user_id with replyLikes table
+				.leftJoin("users as liker", "liker.id", "r_l.user_id")
+				.leftJoin("replies as r", "r.id", "r_l.reply_id")
+				// replier shares user_id with replies table
+				.leftJoin("users as replier", "replier.id", "r.user_id")
+				.leftJoin("posts as p", "p.id", "r.post_id")
+				.select(
+					"r_l.id",
+					"p.group_id",
+					"p.post_content",
+					"r.reply_content",
+					"liker.id as liker_id",
+					"replier.id as replier_id",
+					"r_l.reply_id",
+					"r_l.created_at",
+					"r_l.updated_at",
+					"liker.first_name as liker_first_name",
+					"liker.last_name as liker_last_name",
+					"liker.image as liker_image",
+					"replier.first_name as replier_first_name",
+					"replier.last_name as replier_last_name",
+					"replier.image as replier_image"
+				)
+				.where("r_l.created_at", ">=", ts)
+				.where("p.group_id", filters.group_id)
+		);
 	}
 }
